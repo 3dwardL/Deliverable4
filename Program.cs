09efe162428547779d8b16d920fe1043ff2aeb1d
3dwardL <122574082+3dwardL@users.noreply.github.com>
2023-02-09 12:20:47 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FibonacciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fib = new int[25];
+            fib[0] = 0;
+            fib[1] = 1;
+
+            for (int i = 2; i < fib.Length; i++)
+            {
+                fib[i] = fib[i - 1] + fib[i - 2];
+            }
+
+            for (int i = 0; i < fib.Length; i++)
+            {
+                Console.WriteLine("Fibomacci nunber ({0}) = {1}", i, fib[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
